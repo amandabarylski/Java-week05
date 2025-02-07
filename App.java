0248d53166aada354838f8320d1cc61b0122fea1
Amanda Barylski <amandabarylski@live.com>
2025-02-07 17:42:39 -0500

    finishing touches

--- App.java
+++ App.java
@@ -7,12 +7,20 @@ public class App {
 		Logger asterisks = new AsteriskLogger();
 		//I switched out the strings and ran the code multiple times to check how it looked with different length strings.
 		//My asterisk box looked how I wanted it to, changing to fit the different lengths.
+		System.out.println("Asterisk Logger Tests:");
+		asterisks.log("hello");
+		asterisks.error("hello");
 		asterisks.log("Hello World");
 		asterisks.error("We have a problem!");
+		
+		System.out.println();
 		
 		//Testing the SpacedLogger:
 		Logger spaces = new SpacedLogger();
 		//As with the AsteriskLogger, I checked different string inputs while testing.
+		System.out.println("Spaced Logger Tests:");
+		spaces.log("hello");
+		spaces.error("hello");
 		spaces.log("Good afternoon");
 		spaces.error("Oh no!");
 
